201b9f2a8bd7c792dbf3c1bad37bb934b3b949f7
maenos <66688267+maenos@users.noreply.github.com>
2024-09-06 13:05:58 +0200

    finish

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 import Button from './Button';
 import { useTheme } from '../hooks';
+import { ITheme } from '../constants/types';
 
 // import { Container } from './styles';
 
 interface HotelButtonWithBadgeProps {
-    badgeNumber?: number;
-    text?: string;
-  
-  }
-const Badge: React.FC<HotelButtonWithBadgeProps>= ({badgeNumber, text}) => {
-    const { assets, colors, gradients, sizes } = useTheme();
+  badgeNumber?: number;
+  text?: string;
+  badgeColor: any
+  onPress: () => void;
 
 
-  return <Button flex={1} gradient={gradients.success} marginBottom={sizes.base/3} rounded={false} round={false} style={{marginHorizontal:10,marginTop:3}} >
- {badgeNumber != 0 && <View style={{ position: "absolute", backgroundColor: colors.danger, right: -23, top: -23, margin: 20, width: 30, height: 25, borderRadius: 55, display: "flex", justifyContent: "center", alignItems: "center" }}>
-    <Text style={{ color: "#fff", fontWeight: "bold" }}> {badgeNumber} </Text>
-  </View>}
-  <Text style={{ color: "#fff", fontSize: 20, fontWeight: "bold", textTransform: "uppercase" }}>
+}
+const Badge: React.FC<HotelButtonWithBadgeProps> = ({ badgeNumber, text, badgeColor,onPress }) => {
+  const { assets, colors, gradients, sizes } = useTheme();
 
-   {text}
-  </Text>
 
-</Button>;
+  const buttonGradient = badgeColor && gradients[badgeColor] ? gradients[badgeColor] : gradients.success;
+
+  return <Button flex={1} gradient={buttonGradient} marginBottom={sizes.base / 3} rounded={false} round={false} style={{ marginHorizontal: 10, marginTop: 3 }} onPress={onPress} >
+    {badgeNumber != 0 && <View style={{ position: "absolute", backgroundColor: colors.danger, right: -23, top: -23, margin: 20, width: 30, height: 25, borderRadius: 55, display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "#fff", fontWeight: "bold" }}> {badgeNumber} </Text>
+    </View>}
+    <Text style={{ color: "#fff", fontSize: 20, fontWeight: "bold", textTransform: "uppercase" }}>
+
+      {text}
+    </Text>
+
+  </Button>;
 }
 
 export default Badge;
