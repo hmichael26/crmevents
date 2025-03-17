720667cca87e6c59891d7b6de895dce451b128ef
maenos <66688267+maenos@users.noreply.github.com>
2025-03-17 20:40:19 +0100

    Revert "corrrect"
    This reverts commit 704be131e20a4aca6b8deda05369c6cf3ad59363.

--- app/navigation/Menu.tsx
+++ app/navigation/Menu.tsx
@@ -54,6 +54,8 @@ const ScreensStack = () => {
 
 /* custom drawer menu */
 const DrawerContent = (props: DrawerContentComponentProps) => {
+  const admin = userdata?.user?.admin
+
   const { t, i18n } = useTranslation()
   useEffect(() => {}, [])
 
@@ -90,7 +92,11 @@ const DrawerContent = (props: DrawerContentComponentProps) => {
     // { name: t('screens.extra'), to: 'Extra', icon: assets.extras },
     // { name: t('screens.eventdetails'), to: 'Eventdetails', icon: assets.register },
     // { name: t('screens.eventPresta'), to: 'EventPresta', icon: assets.register },
-    { name: t('screens.presataire'), to: 'Prestataire', icon: assets.register },
+    admin == 1 && {
+      name: t('screens.presataire'),
+      to: 'Prestataire',
+      icon: assets.register,
+    },
     { name: t('screens.clients'), to: 'Client', icon: assets.register },
   ]
 
