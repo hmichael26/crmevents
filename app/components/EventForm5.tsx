d3d192bb3b3155900221ae6b5562b0af2abf6fd2
maenos <66688267+maenos@users.noreply.github.com>
2025-05-27 00:23:13 +0200

    sent

--- app/components/EventForm5.tsx
+++ app/components/EventForm5.tsx
@@ -310,19 +310,18 @@ const Form5: React.FC<Form5Props> = ({ options, onDataChange, item }) => {
         )
     }
   }
-  if (fields === null) {
-    return (
-      <View style={styles.fieldContainer}>
-        <Text
-          style={{ color: colors.danger, fontSize: 20, textAlign: 'center' }}
-        >
-          chargement ...
-        </Text>
-      </View>
-    )
-  }
+
   return (
     <ScrollView style={styles.container}>
+      {(item && item.length > 0 && fields.length === 0) ?? (
+        <View style={styles.fieldContainer}>
+          <Text
+            style={{ color: colors.danger, fontSize: 20, textAlign: 'center' }}
+          >
+            chargement ...
+          </Text>
+        </View>
+      )}
       {fields &&
         fields.map((field, index) => (
           <View key={index} style={styles.fieldContainer}>
