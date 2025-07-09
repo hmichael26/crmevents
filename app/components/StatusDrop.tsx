9b9c89c7bf781c8f394de85753c5e61c37663f26
maenos <66688267+maenos@users.noreply.github.com>
2025-07-09 16:20:22 +0200

    push

--- app/components/StatusDrop.tsx
+++ app/components/StatusDrop.tsx
@@ -29,8 +29,7 @@ const StatusDropdown = ({
       { id: 5, label: 'Nouveau', value: 'Nouveau', color: '#03a9f5' }, // Nouveau
       { id: 7, label: 'À valider', value: 'a_valider', color: '#404fe9' }, // Nouveau statut ajouté
       { id: 1, label: 'À affiner', value: 'a_affiner', color: '#4f37da' }, // À affiner
-
-      { id: 3, label: 'Envoyer', value: 'Envoyer', color: '#690ec2' }, // Envoyer
+      { id: 3, label: 'Envoyé', value: 'Envoyer', color: '#690ec2' }, // Envoyer
       { id: 4, label: 'Hot', value: 'Hot', color: '#f025b6' }, // Hot
     ],
     [],
